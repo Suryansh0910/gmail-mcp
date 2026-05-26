9535be02b9e0ff73f146316381d25b661e70892f
Suryansh0910 <suryansh0910sg@gmail.com>
2026-05-27 01:17:03 +0530

    fix: expose mcp-session-id CORS header

--- src/http.js
+++ src/http.js
@@ -154,6 +154,7 @@ app.use((req, res, next) => {
 app.use((_, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Expose-Headers", "mcp-session-id");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   next();
 });
